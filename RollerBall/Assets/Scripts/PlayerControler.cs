9132a87821a9d81d3bec64c33ed5fc37a0142615
flpserrano <felipeserrano6@gmail.com>
2017-07-22 18:32:15 -0600

    Final version of tutorial

---
+++ RollerBall/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControler : MonoBehaviour {
+
+	Rigidbody rb;
+	public float velocity;
+	int count;
+	public Text points;
+	public Text win;
+	void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		UpdateCount();
+		win.gameObject.SetActive(false);
+	}
+	
+	void FixedUpdate()
+	{
+		float horizontalMovement = Input.GetAxis("Horizontal");
+		float verticalMovement = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3(horizontalMovement,0,verticalMovement);
+		rb.AddForce(movement*velocity);
+	}
+
+	public void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        count = count + 1;
+        UpdateCount();
+		if(count >= 12){
+			win.gameObject.SetActive(true);
+		}
+    }
+
+    private void UpdateCount()
+    {
+        points.text = "Score:" + count;
+    }
+}
